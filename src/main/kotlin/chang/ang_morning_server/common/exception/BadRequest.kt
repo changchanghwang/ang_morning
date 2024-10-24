f1a3b7ef80ed2549090218315e588b17a406a4cd
changchanghwang <hch950627@naver.com>
2024-10-24 11:07:18 +0900

    chore: remove comment

--- src/main/kotlin/chang/ang_morning_server/common/exception/BadRequest.kt
+++ src/main/kotlin/chang/ang_morning_server/common/exception/BadRequest.kt
@@ -2,10 +2,6 @@ package chang.ang_morning_server.common.exception
 
 import org.springframework.http.HttpStatus
 
-/**
- * @param message 서버 에러 메세지
- * @param errorMessage client 에러 메세지
- */
 class BadRequest(
     message: String,
     errorMessage: String?
